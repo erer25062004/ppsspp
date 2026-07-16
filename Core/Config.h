7b710dadbaef974f9454ad037f23f4d3b44d3d51
erer <erer.erer.25062004@gmail.com>
2026-07-16 21:10:52 +0800

    Config.h

--- Core/Config.h
+++ Core/Config.h
@@ -125,7 +125,7 @@ struct TouchControlConfig : public ConfigBlock {
 	ConfigTouchPos touchRightAnalogStick;
 	ConfigTouchPos touchPauseKey;
 
-	enum { CUSTOM_BUTTON_COUNT = 20 };
+	enum { CUSTOM_BUTTON_COUNT = 50 };
 
 	ConfigTouchPos touchCustom[CUSTOM_BUTTON_COUNT];
 
